dc530beb49cb278d9a56aed45f9e62e62c3cc5e2
mbfds <marcelo.florentino.ti@gmail.com>
2018-11-27 11:55:33 -0200

    Projeto Converso binarios/decimal e decimal/binario

---
+++ Conversor/Form1.Designer.cs
@@ -0,0 +1,215 @@
+﻿namespace Conversor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtBinario = new System.Windows.Forms.TextBox();
+            this.txtInteiro = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnCalcular2 = new System.Windows.Forms.Button();
+            this.txtBinario2 = new System.Windows.Forms.TextBox();
+            this.txtInteiro2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(45, 28);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(409, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "CONVERSOR DE NÚMEROS INTEIROS EM BINÁRIO";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(15, 292);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(69, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Valor Inteiro :";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(12, 123);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(72, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Valor Binário :";
+            // 
+            // txtBinario
+            // 
+            this.txtBinario.Location = new System.Drawing.Point(110, 116);
+            this.txtBinario.Name = "txtBinario";
+            this.txtBinario.Size = new System.Drawing.Size(142, 20);
+            this.txtBinario.TabIndex = 3;
+            // 
+            // txtInteiro
+            // 
+            this.txtInteiro.Location = new System.Drawing.Point(110, 285);
+            this.txtInteiro.Name = "txtInteiro";
+            this.txtInteiro.Size = new System.Drawing.Size(142, 20);
+            this.txtInteiro.TabIndex = 4;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(18, 172);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(234, 47);
+            this.btnCalcular.TabIndex = 5;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Location = new System.Drawing.Point(213, 347);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(150, 47);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "Limpar Campos";
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            this.btnLimpar.Click += new System.EventHandler(this.btnLimpar_Click);
+            // 
+            // btnCalcular2
+            // 
+            this.btnCalcular2.Location = new System.Drawing.Point(353, 176);
+            this.btnCalcular2.Name = "btnCalcular2";
+            this.btnCalcular2.Size = new System.Drawing.Size(234, 47);
+            this.btnCalcular2.TabIndex = 12;
+            this.btnCalcular2.Text = "Calcular";
+            this.btnCalcular2.UseVisualStyleBackColor = true;
+            this.btnCalcular2.Click += new System.EventHandler(this.btnCalcular2_Click);
+            // 
+            // txtBinario2
+            // 
+            this.txtBinario2.Location = new System.Drawing.Point(445, 282);
+            this.txtBinario2.Name = "txtBinario2";
+            this.txtBinario2.Size = new System.Drawing.Size(142, 20);
+            this.txtBinario2.TabIndex = 11;
+            // 
+            // txtInteiro2
+            // 
+            this.txtInteiro2.Location = new System.Drawing.Point(445, 116);
+            this.txtInteiro2.Name = "txtInteiro2";
+            this.txtInteiro2.Size = new System.Drawing.Size(142, 20);
+            this.txtInteiro2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(347, 285);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(72, 13);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Valor Binário :";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(350, 120);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(69, 13);
+            this.label5.TabIndex = 8;
+            this.label5.Text = "Valor Inteiro :";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("MS Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(11, 240);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 20);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "RESPOSTA:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("MS Gothic", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(349, 240);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(99, 20);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "RESPOSTA:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.btnCalcular2);
+            this.Controls.Add(this.txtBinario2);
+            this.Controls.Add(this.txtInteiro2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtInteiro);
+            this.Controls.Add(this.txtBinario);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtBinario;
+        private System.Windows.Forms.TextBox txtInteiro;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnCalcular2;
+        private System.Windows.Forms.TextBox txtBinario2;
+        private System.Windows.Forms.TextBox txtInteiro2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Conversor/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Conversor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnLimpar_Click(object sender, EventArgs e)
+        {
+            txtInteiro.Clear();
+            txtBinario.Clear();
+            txtInteiro2.Clear();
+            txtBinario2.Clear();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+
+
+
+			{
+				try
+				{
+					int ValorBinario = Convert.ToInt32(txtBinario.Text);
+
+					txtInteiro.Text = BinarioParaDecimal(ValorBinario);
+				}
+				catch (Exception)
+				{
+
+					MessageBox.Show("Verifique o preenchimento das informações no formulario ! ", "Alerta !", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+				}
+					
+					
+					
+					
+					
+					}
+			
+
+
+		}
+
+		private string BinarioParaDecimal(int ValorBinario)
+		{
+			
+
+			double sum = 0;
+
+			int strn = ValorBinario.ToString().Length; //quantos digitos há no binario
+			for (int i = 0; i < strn; i++)
+			{
+				int lastDigit = ValorBinario % 10; // pega o ultimo digito
+				sum = sum + lastDigit * (Math.Pow(2, i));
+				ValorBinario = ValorBinario / 10; //remove o ultimo digito
+			}
+			
+			return sum.ToString();
+
+		}
+
+        private void btnCalcular2_Click(object sender, EventArgs e)
+        {
+
+            {
+                try
+                {
+                    //Armazena o valor em decimal, convertendo o texto em tipo inteiro
+                    int ValorDecimal = int.Parse(txtInteiro2.Text);
+
+                    //Realiza a chamada do metodo e armazena resultado na textbox
+                    txtBinario2.Text = DecimalParaBinario(ValorDecimal);
+                }
+                catch (Exception)
+                {
+                    MessageBox.Show("Verifique o preenchimento das informações no formulario ! ", "Alerta !", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+                }
+            }
+
+        }
+
+        private string DecimalParaBinario(int ValorDecimal)
+        {
+
+            int resto;
+            var result = "";
+            while (ValorDecimal > 0)
+            {
+                resto = ValorDecimal % 2;
+                ValorDecimal /= 2;
+                result = resto.ToString() + result;
+            }
+            return result.ToString();
+           
+        }
+    }
+    
+
+    }
+
